b20ddd4c53f5bb1b0a2dba1629d86c70a7641ee7
shivamrai83 <shivamrai83@gmail.com>
2021-02-12 16:40:42 +0530

    LogIn-SingUp added

--- front-end/src/LogIn-SingUp/LogIn.jsx
+++ front-end/src/LogIn-SingUp/LogIn.jsx
@@ -2,10 +2,40 @@ import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 
  function LogIn(props) {
-    const { handleSubmit } = props
+    const { handleSubmit, pristine, reset, submitting } = props
     return (
         <form onSubmit={handleSubmit}>
-             <Field name="email"  component="input" type="text" label="E-MAIL" />
+        <div>
+        <label>Email</label>
+        <div>
+          <Field
+            name="email"
+            component="input"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+      </div>
+
+      <div>
+        <label>Password</label>
+        <div>
+          <Field
+            name="password"
+            component="input"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+      <div>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
         </form>
     )
 }
--- front-end/src/LogIn-SingUp/SingUp.jsx
+++ front-end/src/LogIn-SingUp/SingUp.jsx
@@ -1,9 +1,70 @@
 import React from 'react'
+import { Field, reduxForm } from 'redux-form'
 
-export default function SingUp() {
-    return (
+const SimpleForm = props => {
+  const { handleSubmit, pristine, reset, submitting } = props
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label>First Name</label>
         <div>
-            <h1>singup</h1>
+          <Field
+            name="fullName"
+            component="input"
+            type="text"
+            placeholder="Full Name"
+          />
         </div>
-    )
+      </div>
+     
+      <div>
+        <label>Email</label>
+        <div>
+          <Field
+            name="email"
+            component="input"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+      </div>
+
+      <div>
+        <label>Password</label>
+        <div>
+          <Field
+            name="password"
+            component="input"
+            type="password"
+            placeholder="Password"
+          />
+        </div>
+      </div>
+
+      <div>
+        <label>Phone Number</label>
+        <div>
+          <Field
+            name="Phone"
+            component="input"
+            type="number"
+            placeholder="Phone Number.."
+          />
+        </div>
+      </div>  
+
+      <div>
+        <button type="submit" disabled={pristine || submitting}>
+          Submit
+        </button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
+    </form>
+  )
 }
+
+export default reduxForm({
+  form: 'singup' 
+})(SimpleForm)
