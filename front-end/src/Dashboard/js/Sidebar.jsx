fdf6e7797dccd9d3f7fc9c161c4ef52c19512903
shivamrai83 <shivamrai83@gmail.com>
2021-04-02 16:14:28 +0530

    naming changed

--- front-end/src/Dashboard/js/Sidebar.jsx
+++ front-end/src/Dashboard/js/Sidebar.jsx
@@ -63,7 +63,7 @@ function Navigator(props) {
     <Drawer variant="permanent" {...other}>
       <List disablePadding>
         <ListItem className={clsx(classes.firebase, classes.item, classes.itemCategory)}>
-          LocalHost
+          Ancester
         </ListItem>
         <ListItem className={clsx(classes.item, classes.itemCategory)}>
           <ListItemIcon className={classes.itemIcon}>
--- front-end/src/Dashboard/js/Video.jsx
+++ front-end/src/Dashboard/js/Video.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { ReactVideo } from "reactjs-media";
 import Context from '../DashboardContext';
@@ -13,15 +13,12 @@ const styles = (theme) => ({
 
 function Content() {
   const {categories, videoId, } = useContext(Context);
-  const [url, setUrl] = React.useState();
+  const [url, setUrl] = useState();
 
   const videoUrl = (videoId) => {
     const id = categories[0].children[0].id;
     if (videoId === id) {
       return categories[0].children[0].video
-    }
-    else{
-      return "https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
     }
   };
   
@@ -29,14 +26,12 @@ function Content() {
     setUrl(videoUrl(videoId));
   },[videoId])
 
-  console.log("Video",url)
-  console.log("in video id is",categories[0].children[0].video)
+  console.log("Video url",url)
+  
   return (
     <div>
       <ReactVideo
-        // src="https://cdn.videvo.net/videvo_files/video/premium/video0007/small_watermarked/black_headz_spin4k05_preview.webm"
-        // src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
-        src={url}
+        src={url ? url : "https://media.w3.org/2010/05/sintel/trailer_hd.mp4"}
         primaryColor="yellow"
       />
     </div>
