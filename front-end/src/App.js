d2c035809a1e4f5d1056cd035991598139957048
shivamrai83 <shivamrai83@gmail.com>
2021-04-05 11:03:08 +0530

    changing routes of node & js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -8,6 +8,8 @@ import Album from './Dashboard/album'
 import LogIn from './LogIn-SingUp/login'
 import SingUp from './LogIn-SingUp/singup'
 import JS from './Dashboard/js/javascript'
+import Node from './Dashboard/Node/Node'
+import Reacts from './Dashboard/React/React'
 function App() {
   return (
     <div className="App">
@@ -20,11 +22,17 @@ function App() {
             <Route exact path="/singUp">
               <SingUp/>
             </Route>
+            <Route exact path="/album">
+              <Album/>
+            </Route>
             <Route exact path="/js">
               <JS/>
             </Route>
-            <Route exact path="/album">
-              <Album/>
+            <Route exact path="/react">
+              <Reacts/>
+            </Route>
+            <Route exact path="/node">
+              <Node/>
             </Route>
           </Switch>
           </Router>
