b269a7ce22f7ba698eeed0845dbb679f9c065d3c
shivamrai83 <shivamrai83@gmail.com>
2021-04-30 21:07:07 +0530

    Login Singup Copyright Changed

--- front-end/src/LogIn-SingUp/singup.js
+++ front-end/src/LogIn-SingUp/singup.js
@@ -19,7 +19,7 @@ function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
+      <Link color="inherit" href="https://github.com/shivamrai83">
         Shivam Rai
       </Link>{" "}
       {new Date().getFullYear()}
